cd1f5df6a80edd9bf672351f4a5034885f9b3966
Devopuba <charlesopuba3@gmail.com>
2023-04-10 17:11:07 +0300

    initial

---
+++ main.js
@@ -0,0 +1,17 @@
+//Javascript is a programming language designed to interact with elements of web pages//
+//Js consists of ECMAScript-provides the core functionality,Document object model(DOM),interfaces for interacting with elements on web pages,B.O.M(browser object model),provides the browser API for interacting with the web browser
+
+//Syntax-including whitespace,statements,identifiers,keywords,expressions and comments
+
+//Comments are statements not executed by the interpreter
+// var a = "this is a variable defined in a statement"
+
+/*
+This is a multi line comment,
+*/
+
+//variables
+//Can be declared in 4 ways using var,let,const and nothing,they are containers for storing
+//data
+//Declare a variable
+var message;
